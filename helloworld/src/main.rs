31a92d98519379437633dbadb71e027c79e12e17
Divyam Mehta <divyamsandipbhai.mehta@phoenix.edu>
2024-06-06 19:49:50 -0700

    small updates

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -10,13 +10,15 @@ fn main() {
 
     let small_int: i8 = 5; //signed integer 8 bit
     let small_uint: u8 = 5; //unsigned integer 8 bit
-    //default int in Rust is i32
+                            //default int in Rust is i32
     let small_float: f32 = 5.0; //32 bit float.  The . is mandatory
     let large_float: f64 = 5.0; //64 bit float
 
     println!("Min and max of i8: {} to {}", i8::MIN, i8::MAX);
 
     let some_char: char = 'a'; //single character, single quote
+
+    let autoselectint: isize = 1000; //automatically selects int size based on system architecture
 
     println!("Hello, World!");
 }
